c9b4b037182df09ad7ed8577738882eab9db836d
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2023-07-17 20:28:20 +0300

    ESLint and Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {Test1Component} from "src/app/test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './user/components/test4/test4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component
   ],
   imports: [
     BrowserModule,
@@ -15,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
